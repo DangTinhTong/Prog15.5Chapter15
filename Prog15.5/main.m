da55fa2ace870d97694939ac5ab8760d75cf3f72
admin <dangtinhtong@gmail.com>
2017-12-09 23:50:07 +0700

    First commit

--- Prog15.5/main.m
+++ Prog15.5/main.m
@@ -11,7 +11,30 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        //NSLog(@"Hello, World!");
+        NSString *myString1 = @"This is string A";
+        NSString *search, *replace;
+        NSMutableString *myString;
+        NSRange *subString;
+        
+        
+        // Create mutable string from nonmutable
+        myString = [NSMutableString stringWithString:myString1];
+        NSLog(@"%@:",myString);
+        
+        // Insert characters
+        [myString insertString:@"Mutable" atIndex:7];
+        NSLog(@"%@",myString);
+        
+        
+        // Effective concatentation if insert at end
+        [myString insertString:@"and String B" atIndex:[myString length]];
+        NSLog(@"%@",myString);
+        
+        //Or can use appendString directly
+        [myString appendString:@"and String C"];
+        NSLog(@"%@",myString);
+        
     }
     return 0;
 }
